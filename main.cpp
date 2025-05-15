351e4a491f0413e8ceddb80f8fe33956e4934336
snej55 <jenskromdijk@gmail.com>
2025-05-15 20:28:22 +0100

    Load vertices in vertex array

--- main.cpp
+++ main.cpp
@@ -8,8 +8,7 @@ int main()
     PaperLoader paperLoader{};
     paperLoader.loadFromFile("data/papers_with_labels.csv");
 
-    std::vector<float> vertices;
-    paperLoader.getVertices(vertices);
+    std::cout << "Loaded papers!\n";
 
     // ---- OpenGL ---- //
     // initialization
@@ -18,15 +17,39 @@ int main()
     app.setCameraEnabled(true);
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+    // load vertices from paper
+    std::vector<float> vertices;
+    paperLoader.getVertices(vertices);
+
+    // create vertex array and vertex buffer for vertices
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, static_cast<GLsizeiptr>(vertices.size() * sizeof(vertices[0])), vertices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), reinterpret_cast<void*>(0));
+    glEnableVertexAttribArray(0);
+
+    // load shader
+    Shader shader{"shaders/points.vert", "shaders/points.frag"};
+
     // main loop
     while (!app.shouldClose()) {
         app.handleInput();
         app.clear();
 
+        shader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_POINTS, 0, vertices.size());
+
         app.tick();
     }
 
     // clean up
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
     app.close();
 
     return 0;
