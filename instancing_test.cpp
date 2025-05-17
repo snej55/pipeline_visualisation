876ca38bb83269fd017371c8e7ac8226347cdc2f
snej55 <jenskromdijk@gmail.com>
2025-05-17 16:39:48 +0100

    Work on instancing

--- instancing_test.cpp
+++ instancing_test.cpp
@@ -9,6 +9,55 @@ int main()
     app.setCameraEnabled(true);
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+    // quad vertex data to index
+    float quadVertices[] = {
+        // positions     // colors
+        -0.05f,  0.05f,  1.0f, 0.0f, 0.0f,
+        0.05f, -0.05f,  0.0f, 1.0f, 0.0f,
+        -0.05f, -0.05f,  0.0f, 0.0f, 1.0f,
+
+        -0.05f,  0.05f,  1.0f, 0.0f, 0.0f,
+        0.05f, -0.05f,  0.0f, 1.0f, 0.0f,   
+        0.05f,  0.05f,  0.0f, 1.0f, 1.0f		    		
+    };
+
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(0));
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glm::vec2 translations[100];
+    int index {0};
+    constexpr float offset {0.1f};
+    for(int y = -10; y < 10; y += 2)
+    {
+        for(int x = -10; x < 10; x += 2)
+        {
+            glm::vec2 translation;
+            translation.x = static_cast<float>(x) / 10.0f + offset;
+            translation.y = static_cast<float>(y) / 10.0f + offset;
+            translations[index++] = translation;
+        }
+    }
+
+    const Shader shader{"shaders/quad.vert", "shaders/quad.frag"};
+    shader.use();
+
+    for (unsigned int i {0}; i < 100; ++i)
+    {
+        shader.setVec2("offset[" + std::to_string(i) + "]", translations[i]);
+    }
+
     // main loop
     while (!app.shouldClose()) {
         app.handleInput();
