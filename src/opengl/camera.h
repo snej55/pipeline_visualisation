41e9d26224714274c8696d7997490acc33c7ba4b
snej55 <jenskromdijk@gmail.com>
2025-05-18 16:16:30 +0100

    Move camera back

--- src/opengl/camera.h
+++ src/opengl/camera.h
@@ -32,7 +32,7 @@ class Camera
 public:
     Camera()
     {
-        _position = glm::vec3(0.0f, 0.0f, 3.0f);
+        _position = glm::vec3(0.0f, 0.0f, 30.0f);
         _front = glm::vec3(0.0f, 0.0f, -1.0f);
         _up = glm::vec3(0.0f, 1.0f, 0.0f);
         _worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
