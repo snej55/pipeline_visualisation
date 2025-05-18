d3d1368dc7e0a5bb79e915345635d25371e61d5b
snej55 <jenskromdijk@gmail.com>
2025-05-18 14:59:29 +0100

    Work on instanced rendering

--- main.cpp
+++ main.cpp
@@ -19,8 +19,16 @@ int main()
     glEnable(GL_PROGRAM_POINT_SIZE);
 
     // load vertices from paper
-    std::vector<float> vertices;
-    paperLoader.getVertices(vertices, 1.0);
+    std::vector<float> vertices {0.0f, 0.0f, 0.0f};
+    std::vector<float> paperData;
+    paperLoader.getVertices(paperData, 1.0);
+
+    // generate vbo for paper instances
+    unsigned int instanceVBO;
+    glGenBuffers(1, &instanceVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+    glBufferData(GL_ARRAY_BUFFER, static_cast<GLsizeiptr>(paperData.size() * sizeof(paperData[0])), paperData.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     // create vertex array and vertex buffer for vertices
     unsigned int VAO, VBO;
@@ -32,10 +40,16 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, static_cast<GLsizeiptr>(vertices.size() * sizeof(vertices[0])), vertices.data(), GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 4 * sizeof(float), reinterpret_cast<void*>(0));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), reinterpret_cast<void*>(0));
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 4 * sizeof(float), reinterpret_cast<void*>(1));
+    // set instance data
     glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 4 * sizeof(float), reinterpret_cast<void*>(0));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, 4 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
+    glVertexAttribDivisor(1, 1); // update this index every 1th instance
+    glVertexAttribDivisor(2, 1); // "" ""
 
     // load shader
     const Shader shader{"shaders/points.vert", "shaders/points.frag"};
@@ -57,7 +71,7 @@ int main()
         shader.setVec3("camerapos", app.getCameraPosition());
         shader.setFloat("time", static_cast<float>(glfwGetTime()));
         glBindVertexArray(VAO);
-        glDrawArrays(GL_POINTS, 0, vertices.size());
+        glDrawArraysInstanced(GL_POINTS, 0, vertices.size(), paperData.size());
 
         app.disablePostProcessing();
 
--- src/shaders/points.vert
+++ src/shaders/points.vert
@@ -1,6 +1,7 @@
 #version 410 core
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in float aIncluded;
+layout (location = 1) in vec3 aOffset;
+layout (location = 2) in float aIncluded;
 
 uniform mat4 projection;
 uniform mat4 view;
@@ -21,7 +22,7 @@ void main()
 {
     vs_out.Included = aIncluded;
     vs_out.CameraPos = camerapos;
-    vs_out.FragPos = vec3(model * vec4(aPos, 1.0));
+    vs_out.FragPos = vec3(model * vec4(aPos + aOffset, 1.0));
     vs_out.Time = time;
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    gl_Position = projection * view * model * vec4(aPos + aOffset, 1.0);
 }
