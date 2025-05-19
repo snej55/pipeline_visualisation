21e0f80a918b2479c6b7e4b208d441a4653ff39a
snej55 <jenskromdijk@gmail.com>
2025-05-19 09:50:19 +0100

    Setup font manager

--- src/opengl/fonts.h
+++ src/opengl/fonts.h
@@ -12,7 +12,21 @@ public:
     explicit FontManager() = default;
     ~FontManager();
 
-    bool init();
+    bool init(const std::string& font);
+
+    // getters & setters for font face and library
+    [[nodiscard]] const FT_Face& getFace() const {return m_face;}
+    [[nodiscard]] const FT_Library& getLibrary() const {return m_FT;}
+
+    // font path and loaded flag
+    [[nodiscard]] std::string_view getFontPath() const {return m_fontPath;}
+
+private:
+    FT_Face m_face{}; // font face
+    FT_Library m_FT{}; // freetype library
+
+    std::string m_fontPath{""}; // path of the current font
+    bool m_loaded{false}; // whether a font has been loaded or not
 };
 
 #endif
