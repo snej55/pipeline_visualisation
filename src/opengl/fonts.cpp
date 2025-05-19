332c7f5a8df4c8186aab7d425c33205894e5fb39
snej55 <jenskromdijk@gmail.com>
2025-05-19 10:22:43 +0100

    Add font loading

---
+++ src/opengl/fonts.cpp
@@ -0,0 +1,76 @@
+#include "fonts.h"
+
+#include <iostream>
+
+FontManager::~FontManager()
+{
+    free();
+}
+
+bool FontManager::init(const std::string& path, const int height)
+{
+    m_loaded = false;
+    // initialize freetype2 library
+    if (FT_Init_FreeType(&m_FT))
+    {
+        std::cout << "ERROR::FONT_MANAGER: Could not init FreeType library" << std::endl;
+        return -1;
+    }
+
+    // load font
+    if (FT_New_Face(m_FT, path.c_str(), 0, &m_face))
+    {
+        std::cout << "ERROR::FONT_MANAGER: Failed to load font at `" << path << "`" << std::endl;
+        return -1;
+    }
+
+    // set font size
+    FT_Set_Pixel_Sizes(m_face, 0, height);
+    
+    // disable byte-alignment restriction
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    // first 128 ascii characters
+    for (unsigned char c{0}; c < 128; c++)
+    {
+        // load character glyph
+        if (FT_Load_Char(m_face, c, FT_LOAD_RENDER))
+        {
+            std::cout << "ERROR::FONT_MANAGER: Failed to load glyph" << std::endl;
+            continue; // go to next character
+        }
+
+        // generate the texture
+        unsigned int tex;
+        glGenTextures(1, &tex);
+        glBindTexture(GL_TEXTURE_2D, tex);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, m_face->glyph->bitmap.width, m_face->glyph->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE, m_face->glyph->bitmap.buffer);
+        // texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        // store the character in character map
+        Character character {
+            tex,
+            glm::ivec2{m_face->glyph->bitmap.width, m_face->glyph->bitmap.rows},
+            glm::ivec2{m_face->glyph->bitmap_left, m_face->glyph->bitmap_top},
+            static_cast<unsigned int>(m_face->glyph->advance.x)
+        };
+        // insert character into map
+        m_characters.insert(std::pair<char, Character>{c, character});
+    }
+
+    // all good
+    m_loaded = true;
+    return 0;
+}
+
+void FontManager::free()
+{
+    if (m_loaded)
+    {
+        FT_Done_Face(m_face);
+        FT_Done_FreeType(m_FT);
+    }
+}
--- src/opengl/fonts.h
+++ src/opengl/fonts.h
@@ -1,10 +1,21 @@
 #ifndef FONTS_H
 #define FONTS_H
 
+#include <glad/glad.h>
 #include <ft2build.h>
 #include FT_FREETYPE_H
+#include <glm/glm.hpp>
 
 #include <string>
+#include <map>
+
+struct Character 
+{
+    unsigned int textureID;
+    glm::ivec2 size;
+    glm::ivec2 bearing;
+    unsigned int advance;
+};
 
 class FontManager
 {
@@ -12,7 +23,8 @@ public:
     explicit FontManager() = default;
     ~FontManager();
 
-    bool init(const std::string& font);
+    bool init(const std::string& font, int height);
+    void free();
 
     // getters & setters for font face and library
     [[nodiscard]] const FT_Face& getFace() const {return m_face;}
@@ -27,6 +39,8 @@ private:
 
     std::string m_fontPath{""}; // path of the current font
     bool m_loaded{false}; // whether a font has been loaded or not
+
+    std::map<char, Character> m_characters{};
 };
 
 #endif
