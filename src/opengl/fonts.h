61e9ad256d75e7ff46927facfc3fec11a199bf1f
snej55 <jenskromdijk@gmail.com>
2025-05-19 09:44:56 +0100

    Build fonts.h

--- src/opengl/fonts.h
+++ src/opengl/fonts.h
@@ -4,4 +4,15 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
+#include <string>
+
+class FontManager
+{
+public:
+    explicit FontManager() = default;
+    ~FontManager();
+
+    bool init();
+};
+
 #endif
