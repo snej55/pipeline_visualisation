cb5a5ff6c2d6c6811d3225aeca71a042a74c86ef
snej55 <jenskromdijk@gmail.com>
2025-05-20 09:31:31 +0100

    Add clusters.h

---
+++ src/clusters.h
@@ -0,0 +1,47 @@
+// header file with necessary components to render clusters
+#ifndef CLUSTERS_H
+#define CLUSTERS_H
+
+#define CONVHULL_3D_ENABLE
+#include "convhull_3d.h"
+
+#include "paper_loader.h"
+
+// namespace for rendering clusters
+namespace Clusters
+{
+    struct ConvexHull
+    {
+        ch_vertex* vertices;
+        int numVertices;
+        int* faceIndices;
+        int numFaces;
+    };
+
+    struct ClusterData
+    {
+        ConvexHull* hull{nullptr};
+        unsigned int VBO{0};
+        unsigned int EBO{0};
+        unsigned int VAO{0};
+    };
+
+    // loads convex hull for clusters and generates EBO, VBO & VAO
+    class ClusterRenderer
+    {
+    public:
+        ClusterRenderer() = default;
+        ~ClusterRenderer();
+    
+        void init(const std::vector<std::map<int, Cluster>>& clusters);
+        void free();
+    
+    private:
+        // flag to know if we need to free or not
+        bool m_loaded{false};
+        // contains cluster data for rendering
+        std::vector<std::map<int, ClusterData>> m_clusters{};
+    };
+};
+
+#endif
